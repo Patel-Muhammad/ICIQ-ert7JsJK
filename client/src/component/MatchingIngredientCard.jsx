f60b085cb6faa8d2a0aa0db2f509b0f3eaf076be
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-03-03 00:31:44 +0530

    Created the entire app

--- client/src/component/MatchingIngredientCard.jsx
+++ client/src/component/MatchingIngredientCard.jsx
@@ -12,7 +12,9 @@ const MatchingIngredientsCard = ({ matchingIngredients }) => {
             </div>
             <div className="ingredient-item-details">
               <p><strong>Description:</strong> {ingredient.description}</p>
-              <p><strong>Health Impact:</strong> {ingredient.health_impact}</p>
+              <p style={{display:"flex"}}>
+                <strong>Health Impact:</strong> <p style={{ backgroundColor: getHealthImpactColor(ingredient.health_impact), padding:"2px 5px", color:"white", borderRadius:"5px" }}>{ingredient.health_impact}</p>
+              </p>
               <p><strong>Rating:</strong> {ingredient.rating}</p>
             </div>
           </div>
@@ -22,6 +24,17 @@ const MatchingIngredientsCard = ({ matchingIngredients }) => {
       )}
     </div>
   );
+};
+
+const getHealthImpactColor = (healthImpact) => {
+  // Customize the conditions based on your requirements
+  if (healthImpact.toLowerCase() === 'unhealthy') {
+    return 'red';
+  } else if (healthImpact.toLowerCase() === 'controversial') {
+    return 'darkorange';
+  } else {
+    return 'green';
+  }
 };
 
 export default MatchingIngredientsCard;
