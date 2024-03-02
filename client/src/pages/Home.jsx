5299d4900ecdb618c4e668292d5c501b074bb1d5
Amar kumar <aknbo30@gmail.com>
2024-03-02 22:13:07 +0530

    intical plate

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,8 @@
-import { MdOutlineDocumentScanner } from "react-icons/md";
-import Card from "../../components/partial/card";
-import Heading from "../../components/partial/heading";
-import IconCard from "../../components/IconCard";
-import TextScanner from "../../components/partial/TextScanner";
-import HashLoader from "react-spinners/HashLoader";
 
 const Home = () => {
   return (
     <div>
-      <Heading type="h2" fontWeight="light">
-        THis is custom Heading H1 thin
-      </Heading>
-      <TextScanner />
-
-      <div className="flex justify-center items-center h-screen">
-        <HashLoader color="#00DB5F" size={60} speedMultiplier={1} />
-      </div>
+      
     </div>
   );
 };
